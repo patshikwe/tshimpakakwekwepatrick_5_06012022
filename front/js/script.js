1011bca4e4071e4bfe8271a553e36fa55a7a7120
patshikwe <patshikwe@yahoo.fr>
2022-01-24 23:24:33 +0100

    Les 8 produits sont affichés sur la page d'accueil.

--- front/js/script.js
+++ front/js/script.js
@@ -1,42 +1,16 @@
-/* Create element(a) child of element id= "items"
-crée l'élément(a) enfant de l'élément id= "items"*/
-function createLink(){
-    const link = document.createElement("a");
-    const continent = document.getElementById("items");
-    continent.appendChild(link);
-
-    continent.innerHTML = '<a href="./product.html?id=42"></a>';
-}
-
-// createLink();
-// createLink();
-// createLink();
-// createLink();
-// createLink();
-// createLink();
-// createLink();
-// createLink();
-// =====================================
-/* Create element(article) child of element(a)
-crée l'élément(article) enfant de l'élément(a)*/
-
-function createArticle(){
-    let article = document.createElement("article");
-    document.querySelector("section > a").appendChild(article);
+/* Create 8 elements(a) children of element id= "items"
+crée 8 éléments(a) enfants de l'élément id= "items"*/
+// const numberLink = 8
+// for(let i = 0; i < numberLink; i++)  {
+//     let link = document.createElement("a");
+//     const continent = document.getElementById("items");
+//     continent.appendChild(link);
+//     link.className = 'my-link';
     
-    
-}
+    // console.log(link);
+// }
 
-// createArticle();
-// createArticle();
-// createArticle();
-// createArticle();
-// createArticle();
-// createArticle();
-// createArticle();
-// createArticle();
 // ===================================================
-
 
 
 async function fetchProduct() {
@@ -50,77 +24,83 @@ async function fetchProduct() {
     return productData
 }
 
-
 // =====================================
 
-const createImage = async () => {
+
+const createLink = async () => {
    const productData = await fetchProduct();
 
-    document.querySelector("article").innerHTML =  `<img src=
-    "${productData[0].imageUrl}" alt="${productData[0].altTxt}">`;
-    
-    
-    
+   let section = document.getElementById("items");
+   section.innerHTML = `<a href="./product.html?id=${productData[0]._id}">
+   <article>
+     <img src="${productData[0].imageUrl}" alt="${productData[0].altTxt}">
+     <h3 class="productName">${productData[0].name}</h3>
+     <p class="productDescription">${productData[0].description}</p>
+   </article>
+ </a>`;
 
-};
+ section.insertAdjacentHTML('beforeend', `<a href="./product.html?id=${productData[1]._id}">
+ <article>
+   <img src="${productData[1].imageUrl}" alt="${productData[1].altTxt}">
+   <h3 class="productName">${productData[1].name}</h3>
+   <p class="productDescription">${productData[1].description}</p>
+ </article>
+</a>`);
 
-// createImage();
-// createImage();
-// createImage();
-// createImage();
-// createImage();
-// createImage();
-// createImage();
-// createImage();
-// ===============================================
-/* Create element(img) child of element(article)
-crée l'élément(img) enfant de l'élément(article)*/
-/*function createTitleArticle(name){
-    const titleArticle = document.createElement("h3");
-    let article = document.querySelector("article").appendChild(titleArticle);
+section.insertAdjacentHTML('beforeend', `<a href="./product.html?id=${productData[2]._id}">
+<article>
+  <img src="${productData[2].imageUrl}" alt="${productData[2].altTxt}">
+  <h3 class="productName">${productData[2].name}</h3>
+  <p class="productDescription">${productData[2].description}</p>
+</article>
+</a>`);
     
-    article.innerHTML = `${name}`;
-}*/
+section.insertAdjacentHTML('beforeend', `<a href="./product.html?id=${productData[3]._id}">
+<article>
+  <img src="${productData[3].imageUrl}" alt="${productData[3].altTxt}">
+  <h3 class="productName">${productData[3].name}</h3>
+  <p class="productDescription">${productData[3].description}</p>
+</article>
+</a>`);
 
-// createTitleArticle("Kanap Sinopé");
-// createTitleArticle();
-// createTitleArticle();
-// createTitleArticle();
-// createTitleArticle();
-// createTitleArticle();
-// createTitleArticle();
-// createTitleArticle();
-// ==============================================
-/* Create element(img) child of element(article)
-crée l'élément(img) enfant de l'élément(article)*/
-function createParagraph(descrip){
-    const paragraph = document.createElement("p");
-    let article = document.querySelector("article").appendChild(paragraph);
-    
-    article.innerHTML = `${descrip}`;
+section.insertAdjacentHTML('beforeend', `<a href="./product.html?id=${productData[4]._id}">
+<article>
+  <img src="${productData[4].imageUrl}" alt="${productData[4].altTxt}">
+  <h3 class="productName">${productData[4].name}</h3>
+  <p class="productDescription">${productData[4].description}</p>
+</article>
+</a>`);
+
+section.insertAdjacentHTML('beforeend', `<a href="./product.html?id=${productData[5]._id}">
+<article>
+  <img src="${productData[5].imageUrl}" alt="${productData[5].altTxt}">
+  <h3 class="productName">${productData[5].name}</h3>
+  <p class="productDescription">${productData[5].description}</p>
+</article>
+</a>`);
+
+section.insertAdjacentHTML('beforeend', `<a href="./product.html?id=${productData[6]._id}">
+<article>
+  <img src="${productData[6].imageUrl}" alt="${productData[6].altTxt}">
+  <h3 class="productName">${productData[6].name}</h3>
+  <p class="productDescription">${productData[6].description}</p>
+</article>
+</a>`);
+
+section.insertAdjacentHTML('beforeend', `<a href="./product.html?id=${productData[7]._id}">
+<article>
+  <img src="${productData[7].imageUrl}" alt="${productData[7].altTxt}">
+  <h3 class="productName">${productData[7].name}</h3>
+  <p class="productDescription">${productData[7].description}</p>
+</article>
+</a>`);
+
 }
 
-createParagraph("Excepteur sint occaecat cupidatat non proident...");
-// createParagraph();
-// createParagraph();
-// createParagraph();
-// createParagraph();
-// createParagraph();
-// createParagraph();
-// createParagraph();
+// =================================================
 
-// function createImage(source, alt){
-//     fetch("http://localhost:3000/api/products")
-//     .then(response => response.json())
-//     .then(response2 => console.log(response2))
-        
-   
-//  }
 
 function init() {
-    console.log("init")
+    console.log(init)
     createLink()
-    createArticle();
-    
-    createImage();
 }
