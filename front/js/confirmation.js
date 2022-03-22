597500729903119ff1a2502a1d00cf8bf0c95e9a
patshikwe <patshikwe@yahoo.fr>
2022-03-22 17:02:26 +0100

    Modif commentaire

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,10 +1,13 @@
 /**
+ * @param {object} params
+ * @param {string} orderId
+ * URLSearchParams est la méthode pour récupérer l'identifiant via URL.
  */
  let params = new URLSearchParams(window.location.search);
  let orderId = params.get("orderId");
- console.log(orderId);
 // ===========================================================
-/** Fonction pour afficher le numéro de commande 
+/** 
+ * Fonction pour afficher le numéro de commande 
  * orderId est une variable qui a pour valeur le numéro de commande
  * order a pour valeur l'id du paragraphe pour afficher le numéro
  * textcontent est la méthode d'affichage
